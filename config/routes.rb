3f20fd15a43e050f7cea33aef66be8142ac6e8db
Nicolás Galdámez <ngaldamez@lidi.info.unlp.edu.ar>
2017-10-05 23:35:14 -0300

    Set comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'chapter2', to: 'main#chapter2'
   get 'chapter3', to: 'main#chapter3'
   get 'chapter4', to: 'main#chapter4'
+  get 'chapter5', to: 'main#chapter5'
 
   root 'main#index'
 
